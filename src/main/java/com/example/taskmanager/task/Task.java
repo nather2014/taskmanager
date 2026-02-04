680ff8b20a04e34d88b14690d1f65620f91e2792
nather2014 <akhilnather@gmail.com>
2026-02-05 01:28:20 +0530

    update

--- src/main/java/com/example/taskmanager/task/Task.java
+++ src/main/java/com/example/taskmanager/task/Task.java
@@ -5,53 +5,31 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "tasks")
 public class Task {
 
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-  @Column(nullable = false)
-  private String title;
+    @Column(nullable = false)
+    private String title;
 
-  private boolean completed = false;
+    private boolean completed = false;
 
-  private Task task;
+    @ManyToOne
+    @JoinColumn(name = "parent_task_id")
+    private Task parent;
 
-  protected Task(Task task) {
-    this.task = task;
-  }
+    protected Task() {
+    }
 
-  public Task(String title) {
-    this.title = title;
-  }
-
-  public Long getId() {
-    return id;
-  }
-
-  public String getTitle() {
-    return title;
-  }
-
-  public boolean isCompleted() {
-    return completed;
-  }
-
-  public void setCompleted(boolean completed) {
-    this.completed = completed;
-  }
-
-  public void setId(Long id) {
-    this.id = id;
-  }
-
-  public void setTitle(String title) {
-    this.title = title;
-  }
-
+    public Task(String title) {
+        this.title = title;
+    }
 }
